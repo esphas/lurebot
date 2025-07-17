08bf4d8e03c6ec6ebf3c23b4656626fecea774bd
esphas <esphas@hotmail.com>
2025-07-17 16:29:23 +0800

    htkt

--- agents/hitokoto.ts
+++ agents/hitokoto.ts
@@ -10,8 +10,10 @@ export default async (agent: Agent) => {
             const c = mHitokoto[1]?.split(',').map(x => `c=${x.trim()}`).join('&') || 'c=k'
             const response = await fetch(`https://v1.hitokoto.cn/?${c}&encode=json`)
             if (response.ok) {
-                const hitokoto = await response.json()
-                const text = `${hitokoto.hitokoto}\n        ——${hitokoto.from_who || '佚名'}${hitokoto.from ? `(${hitokoto.from})` : ''}`
+                const data = await response.json()
+                const hitokoto = data.hitokoto
+                const from = [data.from_who, data.from].filter(x => x != null).join(' ').trim()
+                const text = `${hitokoto}${from ? `\n            ——${from}` : ''}`
                 await quick.reply(context, text)
             } else {
                 await quick.replyError(context)
